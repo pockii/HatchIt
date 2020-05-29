80da8a50ce3b34ccd0e43ed8dc885d61ea846c86
Clara Cheong <59820998+claracheong4@users.noreply.github.com>
2020-05-29 15:48:42 +0800

    fixed tailwind formatting

--- client/src/postcss.config.js
+++ client/src/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: [
-        require('tailwindcss')('./tailwindcss-config.js'),
+        require('tailwindcss')('./src/tailwindcss.config.js'),
         require('autoprefixer'),
     ],
 }
