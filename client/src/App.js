3ebc90f4dd67adb88f04213f32ec15bd2bbd837b
Clara Cheong <59820998+claracheong4@users.noreply.github.com>
2020-05-29 18:01:35 +0800

    Update App.js
    Added stuff to sign in page

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,18 @@ import logo from './pics/Logo2.png';
 
 function App() {
   return (
-    /*<div class="w-full max-w-xs">
-      <form class="bg-lightbluebg rounded px-8 pt-6 pb-8 mb-4">
-        <label class="block text-darkblue text-sm mb-2" for="name">
-          Username
-        </label>
-        <input class="border rounded w-full py-2 py-3 text-darkblue leading-tight focus:outline-none" 
-          id="name" type="text" placeholder="Username"/>
-      </form>
-    </div>*/
-
-    <div class=" bg-lightbluebg flex items-center grid grid-cols-6 gap-4 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-4xl">    
+    <div class="w-full flex items-center grid grid-cols-6 gap-4 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-4xl">    
       <div class="col-span-3 sm:col-span-2 md:col-span-3 lg:col-span-1 xl:col-span-4 inset-0">
         <img class="object-contain w-full" src={logo} alt="Logo"></img>
       </div>
-      <div class="w-full col-span-3 sm:col-span-2 md:col-span-3 lg:col-span-1 xl:col-span-4">
-        <form class="content-center bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="w-7/12 rounded content center col-span-3 sm:col-span-2 md:col-span-3 lg:col-span-1 xl:col-span-4">
+        <p class="text-center text-darkblue sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+          Sign In
+        </p>
+        <p class="text-center text-darkblue sm:text-xs md:text-sm xl:text-base">
+          {"Here's your chance to care for a pet without the hassle!"}
+        </p>
+        <form class="content-center px-8 pt-6 pb-8 mb-4">
           <div class="mb-4">
           <label class="block text-darkblue sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-2" for="username">
             Username
