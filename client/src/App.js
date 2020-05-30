1c8bd97befbd26a4acf49ae2ed03e7ca398272c7
Kexin <64959565+pockii@users.noreply.github.com>
2020-05-30 15:04:34 +0800

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,27 @@ import logo from './pics/Logo2.png';
 
 function App() {
   return (
-    <div class="bg-lightbluebg w-full flex items-center grid sm:grid-row-8 md:grid-cols-8 gap-2 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-4xl font-curly">    
+    <div class="bg-lightbluebg flex items-center h-screen grid sm:grid-row-8 md:grid-cols-8 gap-2 font-test">    
       <div class="col-span-3 sm:col-span-2 md:col-span-3 lg:col-span-1 xl:col-span-4 inset-0">
         <img class="object-contain w-full" src={logo} alt="Logo"></img>
       </div>
       <div class="w-7/12 rounded content center col-span-3 sm:col-span-2 md:col-span-3 lg:col-span-1 xl:col-span-4">
-        <p class="text-center text-darkblue sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
-          Sign In
-        </p>
-        <p class="text-center text-darkblue sm:text-xs md:text-sm xl:text-base">
-          {"Here's your chance to care for a pet without the hassle!"}
-        </p>
+          <p class="font-semibold text-center text-darkblue sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+            Sign In
+          </p>
+          <p class="text-center text-darkblue sm:text-xs md:text-sm xl:text-base">
+            {"Here's your chance to care for a pet without the hassle!"}
+          </p>
         <form class="content-center px-8 pt-6 pb-8 mb-4">
           <div class="mb-4">
-          <label class="block text-darkblue sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-2" for="username">
+          <label class="font-semibold block text-darkblue sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-1" for="username">
             Username
           </label>
           <input class="appearance-none rounded py-2 px-3 text-gray-700 sm:text-base md:text-lg lg:text-xl xl:text-2xl leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
           </div>
 
           <div class="mb-6">
-            <label class="block text-darkblue sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-2" for="password">
+            <label class="font-semibold block text-darkblue sm:text-lg md:text-xl lg:text-2xl xl:text-3xl mb-1" for="password">
               Password
             </label>
             <input class="appearance-none rounded py-2 px-3 text-gray-m mb-3 sm:text-sm md:text-lg lg:text-xl xl:text-2xl leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******"/>
