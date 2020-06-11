38e23a69d1190e3d81d7f7ffda25b079b3f7a45a
Clara Cheong <59820998+claracheong4@users.noreply.github.com>
2020-06-11 18:55:14 +0800

    Update Account.js
    Add scrollbar

--- client/src/components/home/popups/Account.js
+++ client/src/components/home/popups/Account.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Modal from "react-modal";
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import account from './account.module.css';
 
@@ -49,12 +50,15 @@ export default class Account extends Component {
 
                 <Modal 
                     isOpen={this.state.accountSeen} 
-                    className={account.modal}>
-                        <button 
-                            class="p-3 text-darkblue absolute right-0 top-0 hover:text-gray-500" 
-                            onClick={this.onAccountExitClick}
-                        >
-                            <svg
+                    onRequestClose={this.onAccountExitClick}
+                    shouldCloseOnEsc={true}
+                    className={account.modal}
+                >
+                    <button 
+                        class="p-3 text-darkblue absolute right-0 top-0 hover:text-gray-500" 
+                        onClick={this.onAccountExitClick}
+                    >
+                        <svg
                             class="w-6 fill-current"
                             viewBox="0 0 96 96" 
                             xmlns="http://www.w3.org/2000/svg" 
@@ -64,29 +68,59 @@ export default class Account extends Component {
                                 d="M83.4 21.1 74.9 12.6 48 39.5 21.1 12.6 12.6 21.1 39.5 48 12.6 74.9 21.1 83.4 48 56.5 74.9 83.4 83.4 74.9 56.5 48Z" 
                                 stroke-width="0.229186" 
                                 />
-                            </svg>
-                        </button>
+                         </svg>
+                    </button>
                         
-                        <p class="p-3 text-darkblue sm:text-base md:text-lg lg:text-xl xl:text-2xl inline-flex flex items-center">
-                            <svg 
-                                class="w-6 fill-current"
-                                viewBox="0 0 96 96" 
-                                xmlns="http://www.w3.org/2000/svg" 
-                                xlink="http://www.w3.org/1999/xlink" 
-                                id="Icons_User" 
-                                overflow="hidden">
-                                <g>
+                    <p class="p-3 text-darkblue sm:text-base md:text-lg lg:text-xl xl:text-2xl inline-flex flex items-center">
+                        <svg 
+                            class="w-6 fill-current"
+                            viewBox="0 0 96 96" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            xlink="http://www.w3.org/1999/xlink" 
+                            id="Icons_User" 
+                            overflow="hidden">
+                            <g>
                                 <circle cx="48" cy="30" r="16"/>
                                 <path d="M80 82 80 66C80 63.6 78.8 61.2 76.8 59.6 72.4 56 66.8 53.6 61.2 52 57.2 50.8 52.8 50 48 50 43.6 50 39.2 50.8 34.8 52 29.2 53.6 23.6 56.4 19.2 59.6 17.2 61.2 16 63.6 16 66L16 82 80 82Z"/>
-                                </g>
-                            </svg>
-                            <span>Account</span>
-                        </p>   
-                    <p class="flex justify-center sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">  
-                        <p class="text-darkblue">
-                            Username: {this.props.name}
-                        </p>
-                    </p>         
+                            </g>
+                        </svg>
+                        <span>Account</span>
+                    </p>   
+                    <p class="pl-5 pr-5">
+                        <Scrollbars 
+                            autoHeight
+                            autoHide
+                            autoHideTimeout={1000}
+                            autoHideDuration={400}>
+                            <p class="flex justify-center sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">  
+                                <p class="text-darkblue">
+                                    Username: {this.props.name}
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+                                </p>
+                            </p>         
+                        </Scrollbars>
+                    </p>
                 </Modal>
             </div>
         );
