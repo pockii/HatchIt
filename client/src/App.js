ab2db5b879e46865791f200e8cca62235adcada1
Clara Cheong <59820998+claracheong4@users.noreply.github.com>
2020-06-24 17:51:37 +0800

    Add react-persist

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,9 @@ import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from "react-redux";
-import store from "./store";
+import { store, persistor } from "./store";
 
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
@@ -15,41 +16,43 @@ import Home from "./components/home/Home";
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-    // Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());    
+    // Set auth token header auth
+    const token = localStorage.jwtToken;
+    setAuthToken(token);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(token);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+        // Check for expired token
+    const currentTime = Date.now() / 1000; // to get in milliseconds
+    if (decoded.exp < currentTime) {
+        // Logout user
+        store.dispatch(logoutUser());    
 
-    // Redirect to login
-    window.location.href = "./login";
-  }
+        // Redirect to login
+        window.location.href = "./login";
+    }
 }
 
 export default class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Route exact path="/" component={Login} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Switch>
-              <PrivateRoute exact path="/home" component={Home} />
-            </Switch>
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <Router>
+                        <div className="App">
+                            <Route exact path="/" component={Login} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/register" component={Register} />
+                            <Switch>
+                                <PrivateRoute exact path="/home" component={Home} />
+                            </Switch>
+                        </div>
+                    </Router>
+                  </PersistGate>
+            </Provider>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,34 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
 import rootReducer from "./reducers";
 
 const initialState = {};
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: hardSet
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleware = [thunk];
 
 const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+    persistedReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
-export default store;
+let persistor = persistStore(store);
+
+export {
+    store,
+    persistor
+};
