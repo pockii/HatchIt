1023cd45d3ff246f37d09c7cdbab64bbfe4457ea
Clara Cheong <59820998+claracheong4@users.noreply.github.com>
2020-07-05 20:11:09 +0800

    Update server.js
    Add happiness breakdown api

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const todos = require("./routes/api/todos");
+const happinessbreakdowns = require("./routes/api/happinessbreakdown");
 const app = express();
 
 // Bodyparser middleware
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 app.use(bodyParser.json());
 
@@ -22,15 +23,14 @@ mongoose.set('debug', true);
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { 
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-     }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+    .connect(
+        db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -41,6 +41,7 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/todos", todos);
+app.use("/api/happinessbreakdown", happinessbreakdowns);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
