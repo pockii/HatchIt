e7ea0e2d0b8d5dc499546d9a08402b36bb4f7001
Clara Cheong <59820998+claracheong4@users.noreply.github.com>
2020-07-16 18:21:24 +0800

    Update DraggableIcon.js and FoodWindow.js
    Change from react component to function and change formatting of heights and widths to %

--- client/src/components/home/popups/Food/DraggableIcon.js
+++ client/src/components/home/popups/Food/DraggableIcon.js
@@ -1,47 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class DraggableIcon extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            name: this.props.name,
-            imgsrc: this.props.imgsrc,
-            value: this.props.value
-        };
+export default function DraggableIcon({ name, imgsrc, value, coins }) {
+    function onDragStart(e) {
+        e.dataTransfer.setData("value", value);
+        e.dataTransfer.setData("coins", coins);
     }
 
-    onDragStart = (e) => {
-        console.log("dragstart");
-        e.dataTransfer.setData("value", this.props.value);
-    }
-
-    foodLabel() {
-        if (this.state.name.length > 8) {
-            return  <p class="h-8 pt-1 text-center text-darkblue sm:text-md md:text-lg lg:text-xl xl:text-xl">
-                        {this.state.name}
+    function foodLabel() {
+        if (name.length > 8) {
+            return  <p class="pt-1 text-center text-darkblue sm:text-md md:text-lg lg:text-xl xl:text-xl">
+                        {name}
                     </p>
         } else {
-            return <p class="h-8 text-center text-darkblue sm:text-lg md:text-xl lg:text-2xl xl:text-2xl">
-                        {this.state.name}
+            return <p class="text-center text-darkblue sm:text-lg md:text-xl lg:text-2xl xl:text-2xl">
+                        {name}
                     </p>
         }
     }
 
-    render() {
-        return (
-            <div class="border-solid rounded border-2 border-darkblue bg-yellowbarbg w-32">  
-                <div
-                    onDragStart = {(e) => this.onDragStart(e)} 
-                    draggable="true">
-                        <img class="object-contain w-full h-20 px-2 pt-2"
-                            src={this.state.imgsrc}
-                            alt={this.state.name}>
-                        </img>
-                </div>
-                {this.foodLabel()}
+    return (
+        <div class="flex flex-col flex justify-center border-solid rounded border-2 border-darkblue bg-yellowbarbg w-full h-full">  
+            <div
+                class="w-8/12 self-center"
+                onDragStart = {onDragStart} 
+                draggable="true">
+                    <img class="w-full px-2 pt-2"
+                        src={imgsrc}
+                        alt={name}>
+                    </img>
             </div>
-        )
-    }
-
-
+            {foodLabel()}
+        </div>
+    );
 }
