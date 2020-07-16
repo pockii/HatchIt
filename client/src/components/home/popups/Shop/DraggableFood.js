1f39f5b120de33585da793a987a86bb94a6ccdb5
Clara Cheong <59820998+claracheong4@users.noreply.github.com>
2020-07-16 18:19:30 +0800

    Add files for premium food and shop

---
+++ client/src/components/home/popups/Shop/DraggableFood.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default function DraggableFood({ name, imgsrc, value, coins }) {
+    function onDragStart(e) {
+        e.dataTransfer.setData("value", value);
+        e.dataTransfer.setData("coins", coins);
+    }
+
+    function onDragOver(e) {
+        e.preventDefault();
+    }
+
+    function foodLabel() {
+        if (name.length > 8) {
+            return  <p class="pt-1 text-center text-darkblue sm:text-md md:text-lg lg:text-xl xl:text-xl">
+                        {name}
+                    </p>
+        } else {
+            return <p class="text-center text-darkblue sm:text-lg md:text-xl lg:text-2xl xl:text-2xl">
+                        {name}
+                    </p>
+        }
+    }
+
+    return (
+        <div class="flex flex-col flex justify-center border-solid rounded border-2 border-darkblue bg-yellowbarbg w-full h-full">  
+            <div
+                class="w-4/5 self-center"
+                onDragStart = {onDragStart} 
+                onDragOver = {onDragOver}
+                draggable="true">
+                    <img class="w-full px-2 pt-2"
+                        src={imgsrc}
+                        alt={name}>
+                    </img>
+            </div>
+            {foodLabel()}
+        </div>
+    );
+}
