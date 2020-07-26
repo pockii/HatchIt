794a124137c08c80c59c7d290555ead132e020dd
Clara Cheong <59820998+claracheong4@users.noreply.github.com>
2020-07-26 11:01:54 +0800

    Update InsufficientCoinsError.js and its css module
    Change InsufficientCoinsError Styling

--- client/src/components/home/Coins/InsufficientCoinsError.js
+++ client/src/components/home/Coins/InsufficientCoinsError.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "react-modal";
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import insufficientcoinserror from './insufficientcoinserror.module.css';
 
@@ -30,19 +31,29 @@ export default function InsufficientCoinsError({ insufficientCoinsErrorSeen, onI
                         />
                 </svg>
             </button>
-                
-            <div class="flex flex-col flex content-center p-3 text-darkblue sm:text-base md:text-lg lg:text-xl xl:text-2xl">
-                <svg 
-                    class="w-3/12 fill-current self-center"
-                    viewBox="0 0 96 96" 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    xlink="http://www.w3.org/1999/xlink" 
-                    id="Icons_Warning" 
-                    overflow="hidden">
-                    <path d="M90.6 80 51.5 12C50 9.3 46.1 9.3 44.6 12L5.4 80C3.9 82.7 5.8 86 8.9 86L48 86 87.1 86C90.2 86 92.1 82.7 90.6 80ZM45 28 51 28 51 63 45 63 45 28ZM48 77C45.2 77 43 74.8 43 72 43 69.2 45.2 67 48 67 50.8 67 53 69.2 53 72 53 74.8 50.8 77 48 77Z"/>
-                </svg>
-                <p class="text-center">ERROR: Purchase failed. Insufficient Coins!</p>
-            </div>    
+
+            <div class="p-2 w-full h-full">
+                <Scrollbars 
+                    style={{ height: '100%', width: '100%' }} 
+                    autoHide
+                    autoHideTimeout={1000}
+                    autoHideDuration={400}
+                > 
+                    <div class="flex flex-col flex content-center p-3 text-darkblue sm:text-base md:text-lg lg:text-xl xl:text-2xl w-full">
+                        <svg 
+                            class="w-3/12 fill-current self-center"
+                            viewBox="0 0 96 96" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            xlink="http://www.w3.org/1999/xlink" 
+                            id="Icons_Warning" 
+                            overflow="hidden">
+                            <path d="M90.6 80 51.5 12C50 9.3 46.1 9.3 44.6 12L5.4 80C3.9 82.7 5.8 86 8.9 86L48 86 87.1 86C90.2 86 92.1 82.7 90.6 80ZM45 28 51 28 51 63 45 63 45 28ZM48 77C45.2 77 43 74.8 43 72 43 69.2 45.2 67 48 67 50.8 67 53 69.2 53 72 53 74.8 50.8 77 48 77Z"/>
+                        </svg>
+                        <p class="text-center">ERROR: Purchase failed.</p>
+                        <p class="text-center">Insufficient Coins!</p>
+                    </div>    
+                </Scrollbars>
+            </div>
         </Modal>
     );
 }
